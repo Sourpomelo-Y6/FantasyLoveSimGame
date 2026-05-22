971ad140172f7d0ab2176df58bd0c6178ba2c611
Y6 <hi-y6@hotmail.co.jp>
2026-05-22 17:56:28 +0900

    Fix schedule panel open path

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -118,7 +118,7 @@ public class GameManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.P))
         {
-            schedulePanel.SetActive(true);
+            OpenSchedulePanel();
         }
     }
 
