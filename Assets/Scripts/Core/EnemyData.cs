bec7e25310ed6bda733c6676801ca0424691b6b6
Y6 <hi-y6@hotmail.co.jp>
2026-06-30 07:58:43 +0900

    Add enemy data scaffold for battles

---
+++ Assets/Scripts/Core/EnemyData.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "LoveSim/Enemy Data")]
+public class EnemyData : ScriptableObject
+{
+    public string enemyId = "Enemy";
+    public string displayName = "敵";
+    public BattleStatusData battleStatus = new BattleStatusData();
+    public int rewardMoney = 0;
+    public int affectionChangeOnWin = 0;
+    [TextArea(2, 4)] public string victoryMessage = "戦闘に勝利しました。";
+    [TextArea(2, 4)] public string defeatMessage = "戦闘に敗北しました。";
+
+    public string GetDisplayName()
+    {
+        return !string.IsNullOrEmpty(displayName) ? displayName : enemyId;
+    }
+
+    public BattleStatusData CreateBattleStatus()
+    {
+        return battleStatus != null ? battleStatus.Clone() : new BattleStatusData();
+    }
+}
